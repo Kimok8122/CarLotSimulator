45132395335ec5043ef5ae008af9dff8d6f611ff
Mona Lisa <email@example.com>
2020-09-01 13:11:48 -0600

    starting

--- CarLotSimulator/Car.cs
+++ CarLotSimulator/Car.cs
@@ -5,6 +5,29 @@ namespace CarLotSimulator
     {
         public Car()
         {
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+
+        public string Year { set; get; }
+        public string Make { set; get; }
+        public string Model { set; get; }
+        public string EngineNoise { set; get; }
+        public string HonkNoise { set; get; }
+        public string IsDrive { set; get; }
+
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //The methods should take one string parameter: the respective noise property 
+
+        public void MakeEngineNoise()
+        {
+            return 
         }
+
+        public void MakeHonkNoise()
+        {
+
+        }
+
+
     }
+}
 }
